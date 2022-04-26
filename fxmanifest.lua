bb44daa9de6015cfbf83c62684bff046923a7096
Mobius1 <mobius1@gmx.com>
2022-04-26 11:25:51 +0100

    Update `fx_version`

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,4 @@
-fx_version 'adamant'
+fx_version 'cerulean'
 
 game 'gta5'
 
@@ -6,21 +6,21 @@ description 'Xperience - XP Ranking System for FiveM'
 
 author 'Mobius1'
 
-version '0.1.0'
+version '0.1.1'
+
+shared_scripts {
+    'config.lua',
+    'common/ranks.lua',
+    'common/utils.lua',
+}
 
 server_scripts {
     -- '@mysql-async/lib/MySQL.lua',
     '@oxmysql/lib/MySQL.lua',
-    'config.lua',
-    'common/ranks.lua',
-    'common/utils.lua',
     'server/main.lua'
 }
 
 client_scripts {
-    'config.lua',
-    'common/ranks.lua',
-    'common/utils.lua',
     'client/main.lua',
 }
 
@@ -28,8 +28,7 @@ ui_page 'ui/ui.html'
 
 files {
     'ui/ui.html',
-    'ui/fonts/ChaletComprimeCologneSixty.ttf',
-    'ui/css/app.css',
-    'ui/js/class.xperience.js',
-    'ui/js/app.js'
+    'ui/fonts/*.ttf',
+    'ui/css/*.css',
+    'ui/js/*.js'
 }
